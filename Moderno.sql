a766f6e614c23ec484373da5154acbf05c9ba663
adrian <adrian1820@hotmail.com>
2022-08-19 11:19:30 -0500

    u

--- Moderno.sql
+++ Moderno.sql
@@ -8,7 +8,7 @@ PRINT @Date;
 DELETE FROM  MODERNO WHERE RIGHT(Fecha,7) = @Date;
 
 BULK INSERT MODERNO
-FROM 'C:\Proyectos\Peru\BaseDatos\Lanzamiento_Peru\CargaBases\enefeb.csv'
+FROM 'C:\Proyectos\Peru\BaseDatos\Lanzamiento_Peru\CargaBases\MODERNO.csv'
 WITH (FIELDTERMINATOR=';', FIRSTROW=2, CODEPAGE='ACP');
 
 UPDATE MODERNO SET Importe = REPLACE(Importe, ',', '')
@@ -44,9 +44,9 @@ UPDATE MODERNO SET RealTon = 0 WHERE  RealTon IS NULL;
 DELETE FROM MODERNO  WHERE RealTon = 0 and Importe = '0';
 
 
-BULK INSERT PLAN_MODERNO
-FROM 'C:\Proyectos\Peru\BaseDatos\Lanzamiento_Peru\CargaBases\PlanModerno.csv'
-WITH (FIELDTERMINATOR=';', FIRSTROW=2, CODEPAGE='ACP');
+--BULK INSERT PLAN_MODERNO
+--FROM 'C:\Proyectos\Peru\BaseDatos\Lanzamiento_Peru\CargaBases\PlanModerno.csv'
+--WITH (FIELDTERMINATOR=';', FIRSTROW=2, CODEPAGE='ACP');
 
 DELETE FROM PLAN_MODERNO WHERE PlanTon = 0 AND PlanSol = 0
 
@@ -63,25 +63,44 @@ UPDATE PLAN_MODERNO SET Material = TRIM(Material);
 UPDATE PLAN_MODERNO SET CodCliente = TRIM(CodCliente);
 UPDATE PLAN_MODERNO SET Cliente = TRIM(Cliente);
 
-
-
-
-IF OBJECT_ID(N'tempdb..#MODERNO_STAGING') IS NOT NULL DROP TABLE #MODERNO_STAGING;
-
-SELECT A.Fecha, A.Cadena, A.Cliente, A.Departamento, A.Categoria, A.Familia, CONCAT(A.CodAlicorp, ' - ', A.Material) Material,
-	   SUM(CONVERT(FLOAT,A.Importe))/1000 Importe, SUM(A.RealTon) RealTon
-INTO #MODERNO_STAGING
-FROM MODERNO A
-WHERE RIGHT(Fecha,7) = @Date
-GROUP BY A.Fecha, A.Cadena, A.Cliente, A.Departamento, A.Categoria, A.Familia, CONCAT(A.CodAlicorp, ' - ', A.Material) ;
+UPDATE PLAN_MODERNO SET Fecha = REPLACE(Fecha, '.', '/')
 
 SET LANGUAGE SPANISH;
 
-ALTER TABLE #MODERNO_STAGING ALTER COLUMN Fecha DATE; 
+IF OBJECT_ID(N'tempdb..#MODERNO_STAGING') IS NOT NULL DROP TABLE #MODERNO_STAGING;
+
+SELECT CONVERT(DATE, A.Fecha, 103) Fecha, A.Cadena, B.ClienteHomologado, A.Departamento,
+	   CONCAT(C.CodCategoria, '-', C.Categoria) Categoria, CONCAT(C.CodFamilia, '-', C.Familia) Familia, CONCAT(A.CodAlicorp, ' - ', C.Material) Material,
+	   SUM(CONVERT(FLOAT,A.Importe))/1000 RealSol, SUM(A.RealTon) RealTon, 0 PlanTon, 0 PlanSol
+INTO #MODERNO_STAGING
+FROM MODERNO A
+	LEFT JOIN MAESTRO_CLIENTE B ON A.Cliente = B.Cliente
+	LEFT JOIN MAESTRO_ALICORP_MODERNO C ON A.CodAlicorp = C.CodAlicorp
+WHERE RIGHT(A.Fecha,7) = @Date
+GROUP BY CONVERT(DATE, A.Fecha, 103), A.Cadena, B.ClienteHomologado, A.Departamento,
+	     CONCAT(C.CodCategoria, '-', C.Categoria), CONCAT(C.CodFamilia, '-', C.Familia), CONCAT(A.CodAlicorp, ' - ', C.Material) ;
+
+ALTER TABLE #MODERNO_STAGING ALTER COLUMN PlanTon FLOAT;
+ALTER TABLE #MODERNO_STAGING ALTER COLUMN PlanSol FLOAT;
+
+IF OBJECT_ID(N'tempdb..#MODERNO_STAGING2') IS NOT NULL DROP TABLE #MODERNO_STAGING2;
+
+SELECT CONVERT(DATE, A.Fecha, 103) Fecha, 'SIN DATOS PLAN' Cadena, B.ClienteHomologado, 'SIN DATOS PLAN' Departamento,
+	   CONCAT(C.CodCategoria, '-', C.Categoria) Categoria, CONCAT(C.CodFamilia, '-', C.Familia) Familia, CONCAT(A.CodAlicorp, ' - ', C.Material) Material,
+	   0 RealSol, 0 RealTon, SUM(A.PlanTon) PlanTon, SUM(A.PlanSol) PlanSol INTO #MODERNO_STAGING2
+FROM PLAN_MODERNO A
+	LEFT JOIN MAESTRO_CLIENTE B ON A.Cliente = B.Cliente
+	LEFT JOIN MAESTRO_ALICORP_MODERNO C ON A.CodAlicorp = C.CodAlicorp
+WHERE RIGHT(A.Fecha,7) = @Date
+GROUP BY CONVERT(DATE, A.Fecha, 103), B.ClienteHomologado,
+	     CONCAT(C.CodCategoria, '-', C.Categoria), CONCAT(C.CodFamilia, '-', C.Familia), CONCAT(A.CodAlicorp, ' - ', C.Material) ;
 
 SET LANGUAGE US_ENGLISH;
 
---SELECT * FROM #MODERNO_STAGING
+INSERT INTO #MODERNO_STAGING
+SELECT *
+FROM #MODERNO_STAGING2
+
 
 DECLARE @CountRows INT;
 SELECT @CountRows = COUNT(*) FROM #MODERNO_STAGING;
